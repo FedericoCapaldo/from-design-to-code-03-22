95c1992300b0e275fb3dfe764d44115819b74fcd
Julien Da Silva <julien@lewagon.org>
2021-05-22 16:59:43 +0200

    Reworked boilerplate for AJAX search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,14 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all.order(:title)
+    @movies = Movie.order(year: :desc)
+
+    if params[:query].present?
+      @movies = @movies.where('title ILIKE ?', "%#{params[:query]}%")
+    end
+
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: 'list.html', locals: { movies: @movies } }
+    end
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,3 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
 end
