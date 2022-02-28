3e1df2c15ae350a55882ac9fcc1c1370f7860d4c
Federico Capaldo <federico.capaldo1@gmail.com>
2022-02-28 22:16:18 +0000

    actually sending ajax request and handling it proper in rails controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,5 +5,10 @@ class MoviesController < ApplicationController
     if params[:query].present?
       @movies = @movies.where('title ILIKE ?', "%#{params[:query]}%")
     end
+
+    respond_to do |format|
+      format.html
+      format.text { render partial: 'movies/list', locals: { movies: @movies }, formats: [:html] }
+    end
   end
 end
--- app/javascript/controllers/search_movies_controller.js
+++ app/javascript/controllers/search_movies_controller.js
@@ -10,6 +10,11 @@ export default class extends Controller {
   }
 
   update() {
-    console.log(this.inputTarget.value);
+    const requestUrl = `${this.formTarget.action}?query=${this.inputTarget.value}`;
+    fetch(requestUrl, { headers: { "Accept": "text/plain" } })
+      .then(res => res.text())
+      .then((data) => {
+        this.listTarget.outerHTML = data;
+      })
   }
 }
